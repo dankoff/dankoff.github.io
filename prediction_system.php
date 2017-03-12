882924bdecf02f3453bff6c111b9afbbbf633d5d
JynxSp0ck <jynxsp0ck@googlemail.com>
2017-03-12 16:24:28 +0000

    Add Timetable

---
+++ prediction_system.php
@@ -0,0 +1,16 @@
+<?php
+	class FrequencyAnalysis {
+		var $modifiedData;
+		var $frequencyData;
+		var $predictedData;
+		
+		function createFrequencies() {
+			
+		}
+	}
+	
+	var $originalData;
+	var $modifiedData;
+	var $timeStamp;
+	var $predictedData;
+?>
